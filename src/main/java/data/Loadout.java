7b1cf50814e0616e02248fef51e009b7bfc04a24
Danny <Danny@DESKTOP-O2U9A5Q>
2020-02-26 11:37:11 -0500

    added weaponIO read from file and loadout methods

--- src/main/java/data/Loadout.java
+++ src/main/java/data/Loadout.java
@@ -5,10 +5,41 @@
  */
 package data;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author andrei
  */
 public class Loadout {
+    ArrayList<LineItem> items;
+
+    public Loadout() {
+    }
+
+    public Loadout(ArrayList<LineItem> items) {
+        this.items = items;
+    }
+
+    public ArrayList<LineItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<LineItem> items) {
+        this.items = items;
+    }
     
+    public void addItem(LineItem item){
+        this.items.add(item);
+    }
+    public void removeItem(LineItem item){
+        this.items.remove(item);
+    }
+    public double getTotalWeight(){
+        double total = 0;
+        for (LineItem item : this.items) {
+            total += item.getTotalWeight();
+        }
+        return total;
+    }
 }
--- src/main/java/data/WeaponIO.java
+++ src/main/java/data/WeaponIO.java
@@ -5,10 +5,25 @@
  */
 package data;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author andrei
  */
 public class WeaponIO {
-    
+    public static ArrayList<Weapon> getWeapons(String filepath) 
+            throws IOException {
+        ArrayList<Weapon> weapons = new ArrayList();
+        File file = new File(filepath);
+        Scanner read = new Scanner(file);
+        while(read.hasNextLine()){
+            String[] tokens = read.nextLine().split("|");
+            weapons.add(new Weapon(tokens[0], tokens[1], Double.parseDouble(tokens[2])));
+        }
+    return weapons;
+    }
 }
